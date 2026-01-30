f8e3aeac5033efc2c7272a1bd5ec50b37dc5af5b
Assma-IBIKAS <Assma-IBIKAS>
2026-01-30 11:34:47 +0100

    add create_vectorstore function

---
+++ app/RAG/embeddings.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+
+# fonction d'embeddings + stocker dans chroma_db
+def create_vectorstore(chunks):
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    )
+    persist_directory = "./chroma_db"
+    vectorstore = Chroma.from_documents(
+        documents = chunks,
+        embedding = embeddings,
+        persist_directory = persist_directory
+    )
+
+    db_data = vectorstore.get(include = ['documents','embeddings','metadatas'])
+
+    print("=====================================================================")
+    print(len(db_data['documents']))
+    print("=====================================================================")
+    
+    return db_data 
